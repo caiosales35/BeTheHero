8f15d19b6309b4f5aa3be16140eeb37a9b43f654
Caio Cesar de Morais Sales <caio.cesar@catijr.com.br>
2020-05-02 01:19:05 -0300

    Instalando e configuranco nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => response.json({
-    titulo: 'Ola Mundo',
+    titulo: 'Ola Mundo!',
     evento: 'Semana OmniStack 11',
     aluno: 'Caio Sales'
 }));
