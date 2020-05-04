5e42444a010758c26bf9674cce41cbab8110ae2e
Caio Cesar de Morais Sales <caio.cesar@catijr.com.br>
2020-05-04 16:33:08 -0300

    Rota criacao de ongs e modulo de coneccao ao banco de dados

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => response.json({
-    titulo: 'Ola Mundo!',
-    evento: 'Semana OmniStack 11',
-    aluno: 'Caio Sales'
-}));
+routes.post('/ongs', async (request, response) => {
+    const {name, email, whatsapp, city, uf} = request.body;
+    const id = crypto.randomBytes(4).toString('HEX');
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+    });
+    return response.json({id});
+});
 
 module.exports = routes;
