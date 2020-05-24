741c66f7e312d76d89cd47a479dc686a30d4fd59
Caio Cesar de Morais Sales <caio.cesar@catijr.com.br>
2020-05-24 16:45:27 -0300

    Frontend pagina de logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import {FiLogIn} from 'react-icons/fi';
 import './styles.css';
+import herosImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
 
 export default function Logon(){
     return (
-        <h1>Ola</h1>
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg}/>
+                <form>
+                    <h1>Faça seu logon</h1>
+                    <input placeholder="Sua ID"/>
+                    <button className="button" type="submit">Entrar</button>
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041"/> Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+            <img src={herosImg} alt="Heroes"/>
+        </div>
     );
 }
