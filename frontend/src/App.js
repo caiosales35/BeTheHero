8b5d414d0ed7105093237e1e5a3058da7f27af65
Caio Cesar de Morais Sales <caio.cesar@catijr.com.br>
2020-05-24 15:40:30 -0300

    Iniciando pagina de Logon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,10 @@
 import React, {useState} from 'react';
-import Header from './Header';
-
+import Logon from './pages/Logon';
+import './global.css';
 
 function App() {
-  let [counter, setCounter] = useState(0);
-  // retorn Array[valor, funcao de atualizacao do valor]
-
-  function increment(){
-      setCounter(counter+1);
-  } 
-
   return (
-      <div>
-          <Header title="Semana Omnistack">
-                Contador: {counter}
-          </Header>
-          <button onClick={increment}>Incrementar</button>
-      </div>
+      <Logon/>
   )
 }
 
