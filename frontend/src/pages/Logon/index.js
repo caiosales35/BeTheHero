f193179ff7fde215ebba3a1013d90f6fb3404c41
Caio Cesar de Morais Sales <caio.cesar@catijr.com.br>
2020-06-01 00:24:37 -0300

    Pagina de cadastro de ongs

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {FiLogIn} from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
 import './styles.css';
 import herosImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
@@ -13,9 +14,9 @@ export default function Logon(){
                     <h1>Faça seu logon</h1>
                     <input placeholder="Sua ID"/>
                     <button className="button" type="submit">Entrar</button>
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041"/> Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={herosImg} alt="Heroes"/>
