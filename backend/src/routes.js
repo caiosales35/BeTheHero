1d221a35a06d600006ac11b04a3c3889b62f5ddd
Caio Cesar de Morais Sales <caio.cesar@catijr.com.br>
2020-06-02 14:28:47 -0300

    Correcao rota no back end

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -13,7 +13,7 @@ routes.post('/ongs', OngController.create);
 
 routes.get('/profile', ProfileController.index);
 
-routes.get('/incidents', IncidentController.idex;);
+routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
