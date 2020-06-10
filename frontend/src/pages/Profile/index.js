8ff29c90ef4aa337262ffbe8c58135fa392fd706
Caio Cesar de Morais Sales <caio.cesar@catijr.com.br>
2020-06-10 00:16:21 -0300

    Integração de logout

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from  'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import api from '../../services/api';
 import './styles.css';
@@ -7,6 +7,7 @@ import logoImg from '../../assets/logo.svg';
 
 export default function Profile(){
     const [incidents, setIncidents] = useState([]);
+    const history = useHistory();
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
 
@@ -33,13 +34,18 @@ export default function Profile(){
         }
     }
 
+    function handleLogout(){
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero"/>
                 <span>Bem vinda, {ongName}</span>
                 <Link className="button" to="/incients/new">Cadastrar novo caso</Link>
-                <button type="button"><FiPower size={18} color="#E02041" /></button>
+                <button onClick={handleLogout} type="button"><FiPower size={18} color="#E02041" /></button>
             </header>
             <h1>Casos cadastrados</h1>
             <ul>
